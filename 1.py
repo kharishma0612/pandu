653b938fbb034bbe3bdcb96783b3432898a3d61f
Kharishma <kharishma06@gmail.com>
2025-02-09 20:04:58 +0530

    error fix

--- 1.py
+++ 1.py
@@ -9,28 +9,30 @@ story_lines = [
     "And to show how much I care, here is my heart for you... ❤️"
 ]
 
-# Function to manage the story navigation
-def display_story():
-    if "index" not in st.session_state:
-        st.session_state.index = 0
+# Initialize session state
+if 'index' not in st.session_state:
+    st.session_state.index = 0
 
+# Function to display the story
+def display_story():
     # Display the current line of the story
     st.write(story_lines[st.session_state.index])
 
-    # Next button
-    if st.session_state.index < len(story_lines) - 1:
-        if st.button("Next"):
-            st.session_state.index += 1
-            st.experimental_rerun()
+    # Buttons to navigate the story
+    col1, col2 = st.columns([1, 1])
 
-    # Previous button
-    if st.session_state.index > 0:
-        if st.button("Previous"):
-            st.session_state.index -= 1
-            st.experimental_rerun()
+    with col1:
+        if st.session_state.index > 0:
+            if st.button("Previous"):
+                st.session_state.index -= 1
+
+    with col2:
+        if st.session_state.index < len(story_lines) - 1:
+            if st.button("Next"):
+                st.session_state.index += 1
 
 # Title
 st.title("Our Love Story")
 
-# Call the function to display the story
+# Display the story
 display_story()
